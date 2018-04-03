ba561edda741a9d2bcd6efd472d5b4b5e4a1d55b
Jérémy Sernit <jeremy.sernit@telecomnancy.net>
2018-04-03 09:32:59 +0200

    Ajout de la table patient et lésion et les classe associées
    Signed-off-by: YannRicci <yann.ricci@telecomnancy.net>

---
+++ src/table/Patient.java
@@ -0,0 +1,41 @@
+package src.table;
+
+import java.sql.Date;
+
+public class Patient {
+    private int id;
+    private String nom;
+    private String genre;
+    private Date dateNaissance;
+
+    public Patient() {}
+
+
+
+    public Patient(int id, String nom, String genre, Date dateBirth){
+        this.id=id;
+        this.nom=nom;
+        this.genre=genre;
+        this.dateNaissance=dateBirth;
+
+    }
+
+    public int getId(){return this.id;}
+    public String getNom(){return this.nom;}
+    public String getGenre(){return this.genre;}
+    public Date getDateNaissance(){return this.dateNaissance;}
+
+    public void setId(int id){this.id= id;}
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+}
