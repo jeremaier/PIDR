7966d890e90dda19443699b95bbf42bc1bd56075
YannRicci <yann.ricci@telecomnancy.net>
2018-04-14 14:35:30 +0200

    Ajout controlleur AddSite et Lame
    Signed-off-by: YannRicci <yann.ricci@telecomnancy.net>

---
+++ src/controller/AddSiteController.java
@@ -0,0 +1,58 @@
+package src.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddSiteController implements Initializable {
+
+    @FXML
+    TextField siteAnatomique;
+
+    @FXML
+    CheckBox sain;
+
+    @FXML
+    TextField numMesure;
+
+    @FXML
+    TextField orientation;
+
+    @FXML
+    ChoiceBox diag;
+
+    @FXML
+    Button aurteDiag;
+
+    @FXML
+    Button addFicherDiag;
+
+    @FXML
+    Label checkFichierDiag;
+
+    @FXML
+    Button addFichierSpectre;
+
+    @FXML
+    Label checkFichierSpectre;
+
+    @FXML
+    Button addFichierMoy;
+
+    @FXML
+    Label checkFichierMoy;
+
+    @FXML
+    Button annuler;
+
+    @FXML
+    Button ajouter;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+}
---
+++ src/controller/LameController.java
@@ -0,0 +1,36 @@
+package src.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class LameController {
+
+    @FXML
+    Button retour;
+
+    @FXML
+    TableView tab;
+
+    @FXML
+    Button photo;
+
+    @FXML
+    Button modifier;
+
+    @FXML
+    Button ajouter;
+
+    @FXML
+    Button supprimer;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
