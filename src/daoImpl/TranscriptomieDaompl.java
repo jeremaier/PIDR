2591640579ead6d35cd6bab44eec15bb8df0eb4b
Jeremy <jeremy.sernit@free.fr>
2018-04-26 18:58:21 +0200

    Transcriptomie

--- src/daoImpl/TranscriptomieDaompl.java
+++ src/daoImpl/TranscriptomieDaompl.java
@@ -1,9 +1,11 @@
 package src.daoImpl;
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLNonTransientConnectionException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import src.dao.TranscriptomieDao;
 import src.table.TranscriptomicAnalysis;
+import src.utils.FileManager;
 
 import java.sql.*;
 import java.util.List;
@@ -22,7 +24,11 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
             preparedStatement = connection.prepareStatement("INSERT INTO analyse_transcriptomique (ID, ID_SITE_CUTANE, FICHIER_BRUT, FICHIER_CUT, RNA, ARNC, CY3, CONCENTRATION, RENDEMENT, ACTIVITE_SPECIFIQUE, CRITERE_EXCLUSION, NUM_SERIE, NUM_EMPLACEMENT, QUALITY_REPORT)" + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
             preparedStatement = this.setPreparedStatement(preparedStatement, transcr, 1);
             preparedStatement.executeUpdate();
-            System.out.println("INSERT INTO analyse_transcriptomique (ID, ID_SITE_CUTANE, FICHIER_BRUT, FICHIER_CUT, RNA, ARNC, CY3, CONCENTRATION, RENDEMENT, ACTIVITE_SPECIFIQUE, CRITERE_EXCLUSION, NUM_SERIE, NUL_EMPLACEMENT, QUALITY_REPORT)\" + \"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            System.out.println("INSERT INTO analyse_transcriptomique (ID, ID_SITE_CUTANE, FICHIER_BRUT, FICHIER_CUT, RNA, ARNC, CY3, CONCENTRATION, RENDEMENT, ACTIVITE_SPECIFIQUE, CRITERE_EXCLUSION, NUM_SERIE, NUL_EMPLACEMENT, QUALITY_REPORT)");
+        } catch (MySQLNonTransientConnectionException e) {
+            FileManager.openAlert("La connection avec le serveur est interrompue");
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -38,15 +44,20 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
 
     @Override
     public TranscriptomicAnalysis selectById(int id) {
-        TranscriptomicAnalysis transcr = new TranscriptomicAnalysis();
+        TranscriptomicAnalysis transcr = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
         try {
-            preparedStatement = connection.prepareStatement("SELECT * FROM analyse_transcriptomique WHERE ID = ?");
-            preparedStatement.setInt(1, transcr.getId());
+            preparedStatement = connection.prepareStatement("SELECT * FROM analyse_transcriptomique WHERE ID = ? ORDER BY ID");
+            preparedStatement.setInt(1, id);
             resultSet = preparedStatement.executeQuery();
-            transcr = this.addToTranscriptomie(transcr, resultSet);
+            transcr = this.addToTranscriptomie(resultSet);
+
+            System.out.println("SELECT * FROM analyse_transcriptomique WHERE ID ORDER BY ID");
+        } catch (MySQLNonTransientConnectionException e) {
+            FileManager.openAlert("La connection avec le serveur est interrompue");
+            e.printStackTrace();
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
@@ -65,14 +76,6 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
                     e.printStackTrace();
                 }
             }
-
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch(SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
 
         return transcr;
@@ -82,28 +85,33 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
     public List<TranscriptomicAnalysis> selectAll() {
         ObservableList<TranscriptomicAnalysis> transcr = FXCollections.observableArrayList();
         Statement statement = null;
-        ResultSet resultSet;
+        ResultSet resultSet = null;
 
         try {
             statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM analyse_transcriptomique");
-            transcr = this.addToObservableList(transcr, resultSet);
+            resultSet = statement.executeQuery("SELECT * FROM analyse_transcriptomique ORDER BY ID");
 
-            System.out.println("SELECT * FROM site_cutane");
+            while (resultSet.next())
+                transcr.add(this.addToTranscriptomie(resultSet));
+
+            System.out.println("SELECT * FROM site_cutane ORDER BY ID");
+        } catch (MySQLNonTransientConnectionException e) {
+            FileManager.openAlert("La connection avec le serveur est interrompue");
+            e.printStackTrace();
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
-            if(statement != null) {
+            if (resultSet != null) {
                 try {
-                    statement.close();
-                } catch(SQLException e) {
+                    resultSet.close();
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
 
-            if(connection != null) {
+            if (statement != null) {
                 try {
-                    connection.close();
+                    statement.close();
                 } catch(SQLException e) {
                     e.printStackTrace();
                 }
@@ -124,6 +132,9 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
             preparedStatement.executeUpdate();
 
             System.out.println("UPDATE analyse_transcriptomique SET" + "ID_SITE_CUTANE = ?, FICHIER_BRUT = ?, FICHIER_CUT = ?, RNA = ?, ARNC = ?, CY3 = ?, CONCENTRATION =?, RENDEMENT = ?, ACTIVITE_SPECIFIQUE = ?, CRITERE_EXCLUSION = ?, NUM_SERIE = ?, NUM_EMPLACEMENT = ?, NUM_EMPLACEMENT = ?, QUALITY_REPORT =? WHERE ID = ?");
+        } catch (MySQLNonTransientConnectionException e) {
+            FileManager.openAlert("La connection avec le serveur est interrompue");
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -138,70 +149,48 @@ public class TranscriptomieDaompl extends daoImpl implements TranscriptomieDao {
 
     }
 
-    public void delete(int id){
-        this.delete(connection,"analyse_transcriptomique", id);
+    private TranscriptomicAnalysis addToTranscriptomie(ResultSet resultSet) throws SQLException {
+        TranscriptomicAnalysis transcriptomicAnalysis = new TranscriptomicAnalysis();
+
+        transcriptomicAnalysis.setId(resultSet.getInt("ID"));
+        transcriptomicAnalysis.setIdCutaneousSite(resultSet.getInt("ID_SITE_CUTANE"));
+        transcriptomicAnalysis.setFichierBrut(resultSet.getString("FICHIER_BRUT"));
+        transcriptomicAnalysis.setFichierCut(resultSet.getString("FICHIER_CUT"));
+        transcriptomicAnalysis.setRIN(resultSet.getInt("RIN"));
+        transcriptomicAnalysis.setConcentration(resultSet.getDouble("CONCENTRATION"));
+        transcriptomicAnalysis.setARNC(resultSet.getDouble("ARNC"));
+        transcriptomicAnalysis.setCyanine(resultSet.getDouble("CY3"));
+        transcriptomicAnalysis.setSpecificActivity(resultSet.getString("ACTIVITE_SPECIFIQUE"));
+        transcriptomicAnalysis.setExclusionCriteria(resultSet.getString("CRITERE_EXCLUSION"));
+        transcriptomicAnalysis.setSerialNumber(resultSet.getInt("NUM_SERIE"));
+        transcriptomicAnalysis.setLamellaLocation(resultSet.getInt("NUM_EMPLACEMENT"));
+        transcriptomicAnalysis.setQualityReport(resultSet.getString("QUALITY_REPORT"));
+
+        return transcriptomicAnalysis;
     }
 
-    private ObservableList<TranscriptomicAnalysis> addToObservableList(ObservableList<TranscriptomicAnalysis> transcr, ResultSet resultSet) {
-        try {
-            while (resultSet.next())
-                transcr.add(this.addToTranscriptomie(new TranscriptomicAnalysis(), resultSet));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return transcr;
-    }
-
-    private TranscriptomicAnalysis addToTranscriptomie(TranscriptomicAnalysis transcr, ResultSet resultSet) throws SQLException {
-
-        transcr.setId(resultSet.getInt("ID"));
-        transcr.setIdCutaneousSite(resultSet.getInt("ID_SITE_CUTANE"));
-        transcr.setFichierBrut( resultSet.getString("FICHIER_BRUT"));
-        transcr.setFichierCut(resultSet.getString("FICHIER_CUT"));
-        transcr.setRIN(resultSet.getInt("RIN"));
-        transcr.setConcentration(resultSet.getDouble("CONCENTRATION"));
-        transcr.setARNC(resultSet.getDouble("ARNC"));
-        transcr.setCyanine(resultSet.getDouble("CY3"));
-        transcr.setSpecificActivity(resultSet.getString("ACTIVITE_SPECIFIQUE"));
-        transcr.setExclusionCriteria(resultSet.getString("CRITERE_EXCLUSION"));
-        transcr.setSerialNumber(resultSet.getInt("NUM_SERIE"));
-        transcr.setLamellaLocation(resultSet.getInt("NUM_EMPLACEMENT"));
-        transcr.setQualityReport(resultSet.getString("QUALITY_REPORT"));
-        return transcr;
+    public void delete(int id) {
+        this.delete(connection, "analyse_transcriptomique", id);
     }
 
     protected PreparedStatement setPreparedStatement(PreparedStatement preparedStatement, Object object, int indexDebut) throws SQLException {
         if (indexDebut == 1)
             preparedStatement.setInt(indexDebut, ((TranscriptomicAnalysis) object).getId());
 
-        preparedStatement.setInt(indexDebut + 1, ((TranscriptomicAnalysis) object).getId());
-        preparedStatement.setInt(indexDebut + 2, ((TranscriptomicAnalysis) object).getIdCutaneousSite());
-        preparedStatement.setString(indexDebut + 3, ((TranscriptomicAnalysis) object).getFichierBrut());
-        preparedStatement.setString(indexDebut + 4, ((TranscriptomicAnalysis) object).getFichierCut());
-        preparedStatement.setInt(indexDebut + 5, ((TranscriptomicAnalysis) object).getRIN());
-        preparedStatement.setDouble(indexDebut + 6, ((TranscriptomicAnalysis) object).getConcentration());
-        preparedStatement.setDouble(indexDebut + 7, ((TranscriptomicAnalysis) object).getARNC());
-        preparedStatement.setDouble(indexDebut + 8, ((TranscriptomicAnalysis) object).getCyanine());
-        preparedStatement.setDouble(indexDebut + 9, ((TranscriptomicAnalysis) object).getYield());
-        preparedStatement.setString(indexDebut + 10, ((TranscriptomicAnalysis) object).getSpecificActivity());
-        preparedStatement.setString(indexDebut + 11, ((TranscriptomicAnalysis) object).getExclusionCriteria());
-        preparedStatement.setInt(indexDebut + 12, ((TranscriptomicAnalysis) object).getSerialNumber());
-        preparedStatement.setInt(indexDebut + 13, ((TranscriptomicAnalysis) object).getLamellaLocation());
-        preparedStatement.setString(indexDebut + 14, ((TranscriptomicAnalysis) object).getQualityReport());
-
+        preparedStatement.setInt(indexDebut + 1, ((TranscriptomicAnalysis) object).getIdCutaneousSite());
+        preparedStatement.setString(indexDebut + 2, ((TranscriptomicAnalysis) object).getFichierBrut());
+        preparedStatement.setString(indexDebut + 3, ((TranscriptomicAnalysis) object).getFichierCut());
+        preparedStatement.setInt(indexDebut + 4, ((TranscriptomicAnalysis) object).getRIN());
+        preparedStatement.setDouble(indexDebut + 5, ((TranscriptomicAnalysis) object).getConcentration());
+        preparedStatement.setDouble(indexDebut + 6, ((TranscriptomicAnalysis) object).getARNC());
+        preparedStatement.setDouble(indexDebut + 7, ((TranscriptomicAnalysis) object).getCyanine());
+        preparedStatement.setDouble(indexDebut + 8, ((TranscriptomicAnalysis) object).getYield());
+        preparedStatement.setString(indexDebut + 9, ((TranscriptomicAnalysis) object).getSpecificActivity());
+        preparedStatement.setString(indexDebut + 10, ((TranscriptomicAnalysis) object).getExclusionCriteria());
+        preparedStatement.setInt(indexDebut + 11, ((TranscriptomicAnalysis) object).getSerialNumber());
+        preparedStatement.setInt(indexDebut + 12, ((TranscriptomicAnalysis) object).getLamellaLocation());
+        preparedStatement.setString(indexDebut + 13, ((TranscriptomicAnalysis) object).getQualityReport());
 
         return preparedStatement;
     }
-
-
 }
